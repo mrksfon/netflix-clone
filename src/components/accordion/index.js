a86bdec954d0698708b4c67b2f645848471699d9
marenda <markostevic96@hotmail.com>
2020-09-25 00:19:05 +0200

    Creating Accordion Component

---
+++ src/components/accordion/index.js
@@ -0,0 +1,44 @@
+import React,{useState,useContext,createContext} from 'react';
+import {Container,Inner,Title,Item,Header,Body} from './styles/accordion';
+
+
+const ToggleContext = createContext();
+
+export default function Accordion({ children, ...restProps }) {
+    return (
+        <Container {...restProps}>
+            <Inner>{children}</Inner>
+        </Container>
+    )
+};
+
+Accordion.Title = function AccordionTitle({children,...restProps}){
+    return <Title {...restProps}>{children}</Title>;
+};
+
+Accordion.Item = function AccordionItem({children,...restProps}){
+    const [toggleShow,setToogleShow] = useState(false);
+    
+    return(
+        <ToggleContext.Provider value={{toggleShow,setToogleShow}}>
+            <Item  {...restProps}>{children}</Item>
+        </ToggleContext.Provider>
+    );
+};
+
+Accordion.Header = function AccordionHeader({children,...restProps}){
+    const {toggleShow,setToogleShow} = useContext(ToggleContext);
+
+    return(
+        <Header onClick={() => setToogleShow((toggleShow) => !toggleShow)} {...restProps}>
+            {children}
+            {toggleShow ?  (<img src="/images/icons/close-slim.png" alt="Close" />) : (<img src="/images/icons/add.png" alt="Open" />)}
+        </Header>
+    );
+};
+
+Accordion.Body = function AccordionBody({children,...restProps}){
+    const {toggleShow} = useContext(ToggleContext);
+
+    return toggleShow ? <Body {...restProps}>{children}</Body> : null;
+}
